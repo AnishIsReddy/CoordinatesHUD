61969cc73ee114b3c010067cbde884046288405a
Anish Reddy <anishisreddy95@gmail.com>
2022-01-30 01:50:43 -0500

    Added direction
    Added text that displayed the direction the player was facing

--- src/main/java/gg/inferno/coordsdisplay/coordsdisplay.java
+++ src/main/java/gg/inferno/coordsdisplay/coordsdisplay.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 import com.mojang.blaze3d.vertex.PoseStack;
 import org.apache.logging.log4j.LogManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec3;
 
 
@@ -26,8 +27,30 @@ public class coordsdisplay {
         Minecraft mc = Minecraft.getInstance();
         
         Vec3 pos = mc.player.position();
-        String data = "[Position]   X: " + Math.round(pos.x * 10)/10 + "   Y: " + Math.round(pos.y * 10)/10 + "   Z: " + Math.round(pos.z * 10)/10;
-        mc.font.draw(matrix, data, 2, 1, 16777215);
+        String datapos = "[Position]   X: " + Math.round(pos.x * 10)/10 + "   Y: " + Math.round(pos.y * 10)/10 + "   Z: " + Math.round(pos.z * 10)/10;
+
+        Direction bearing = mc.player.getDirection();
+        String dir;
+        switch(bearing) {
+            case NORTH:
+               dir = "Neg Z";
+               break;
+            case SOUTH:
+               dir = "Pos Z";
+               break;
+               case EAST:
+               dir = "Pos X";
+               break;
+            case WEST:
+               dir = "Neg X";
+               break;
+            default:
+               dir = "Invalid";
+            }
+            String bearing_data = "[Bearing]   Facing " + bearing.getName().toUpperCase() + " (" + dir + ")";
+
+        mc.font.draw(matrix, datapos, 2, 1, 16777215);
+        mc.font.draw(matrix, bearing_data, 2, 10, 16777215);
         LogManager.getLogger().info("Event Called");
     }
 }
